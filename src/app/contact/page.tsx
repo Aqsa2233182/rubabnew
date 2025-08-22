f1800af33b1ed30a1ca888ecd4c5e073ea84f2c6
aqsamalik <aqsara5899@gmail.com>
2025-08-22 04:22:01 -0700

    first

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div><figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <img className="w-24 h-24 rounded-full mx-auto" src="/aqsa.jpg" alt="" width="384" height="512"/>
+  <div className="pt-6 space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div>
+        Aqsa rubab
+      </div>
+      <div>
+        Ai engineer
+      </div>
+    </figcaption>
+  </div>
+</figure></div>
+  )
+}
+
+export default page
