466ee725b97b9beaeace09e745ded8f8e385d6b0
aqsamalik <aqsara5899@gmail.com>
2025-08-22 05:00:31 -0700

    my project

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,8 +2,48 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div><figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-  <img className="w-24 h-24 rounded-full mx-auto" src="/aqsa.jpg" alt="" width="384" height="512"/>
+    <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/malik.jpg" alt="" width="384" height="512"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+      aqsa rubab
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+      Ai developer
+      </div>
+    </figcaption>
+  </div>
+</figure>
+<figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/rubab.jpg" alt="" width="384" height="512"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+        My name is aqsa malik i am ai engineer.
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+       Ai Engineer, 
+      </div>
+    </figcaption>
+  </div>
+</figure>
+<figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <img className="w-24 h-24 rounded-full mx-auto" src="/zara.jpg" alt="" width="384" height="512"/>
   <div className="pt-6 space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
